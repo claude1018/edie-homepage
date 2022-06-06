3265149f56e9ce241a6212201a829c29ff896607
claude1018 <ejeraclaude@outlook.ph>
2022-06-06 11:13:00 +0800

    :tada: Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const autoprefixer = require("autoprefixer");
+
+function compileStyles() {
+  return gulp
+    .src("./scss/main.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(gulp.dest("./app/css/"));
+}
+
+exports.compileStyles = compileStyles;
+exports.watch = function () {
+  gulp.watch("./sass/**/*.sass", gulp.series("compileStyles"));
+};
